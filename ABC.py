5934f673a0223e21fd4b7912535159494d713da2
Rahul Singh nagesh <98824774+Hexton09@users.noreply.github.com>
2023-04-18 11:30:48 +0530

    Update ABC.py

--- ABC.py
+++ ABC.py
@@ -23,7 +23,7 @@ table = 'employee'
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('index.html')
+    return render_template('signupme.html')
 
 
 @app.route("/about", methods=['POST'])
